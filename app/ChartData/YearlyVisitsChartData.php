a6d17123c4166076890f4b1bfe593edccd34a8e9
Massay Bah <massaybah@gmail.com>
2023-05-18 12:03:28 +0000

    latest changes

--- app/ChartData/YearlyVisitsChartData.php
+++ app/ChartData/YearlyVisitsChartData.php
@@ -3,6 +3,8 @@
 namespace App\ChartData;
 
 use App\Models\Visit;
+use DateTime;
+use Illuminate\Support\Facades\DB;
 
 class YearlyVisitsChartData
 {
@@ -25,13 +27,50 @@ class YearlyVisitsChartData
         $weekEndDate = $now->copy()->endOfYear()->format('Y-m-d');
 
         $visits = Visit::query()
-            ->selectRaw('MONTHNAME(date) as date1, sum(amount) as count')
+            ->selectRaw('DATE_FORMAT(date,"%M %Y") as date1, sum(amount) as count')
             ->whereDate('date', '>=', $weekStartDate)
             ->whereDate('date', '<=', $weekEndDate)
-            ->orderByRaw('date1')
-            ->groupByRaw('date1')
+            ->orderBy('date1','desc')
+            ->groupBy('date1')
             ->get()
             ->pluck('count', 'date1');
+
+        // $visits = DB::table("visits")
+        //     ->select(DB::raw('EXTRACT(MONTH FROM date) AS month, sum(amount) as count'))
+        //     ->whereDate('date', '>=', $weekStartDate)
+        //     ->whereDate('date', '<=', $weekEndDate)
+        //     ->orderBy('date')
+        //     ->groupBy(DB::raw('month'))
+        //     ->get()
+        //     ->pluck('count', 'month');
+
+        // $visits = Visit::select(DB::raw('SUM(amount) as data'), DB::raw("YEAR(date) year"),DB::raw('MONTH(date) months'),  DB::raw('MONTHNAME(date) month'))
+        // ->groupBy('year')
+        // ->groupBy('months')
+        // ->orderBy('year', 'ASC')
+        // ->orderBy('months', 'ASC')
+        // ->get();
+        // dd($visits);
+
+            // ->map(function($item){
+            //     return [
+            //         'id' => $item['count'],
+            //         'name' => $item['month']
+            //     ];
+            // });
+            //->pluck('count', 'month');
+
+
+        //    dd($visits);
+           // ->pluck('count', 'month');
+
+        //     function($item){
+        //         return $item->date->format('MM');
+        //         //DB::raw('month')
+        //    }
+
+
+            // dd($visits);
 
         $colors = [];
 
@@ -39,13 +78,27 @@ class YearlyVisitsChartData
             $colors[] = self::random_color();
         }
 
+
+        $monthValues = $visits->values()->toArray();
+
+        $monthKeys = $visits->keys()->toArray();
+
+
+
+
+        usort($monthKeys, function($a, $b) {
+            $dateA = DateTime::createFromFormat('M Y', $a);
+            $dateB = DateTime::createFromFormat('M Y', $b);
+            return $dateA <=> $dateB;
+        });
+
         return [
-            'labels' => $visits->keys(),
+            'labels' => $monthKeys,
             'datasets' => array(
                 [
-                    'label' => 'Amount spend of Per Day in '.  $now->format('Y'),
+                    'label' => 'Amount spend of Per Day:',
                     'backgroundColor' => $colors,
-                    'data' => $visits->values()
+                    'data' =>  $monthValues
                 ]
             )
         ];
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -20,26 +20,32 @@ defineProps({
         <div class="py-12">
             <div class="max-w-[90%]  mx-auto sm:px-6 lg:px-8">
 
-              <div class="flex justify-end my-4">
+                <div class="flex justify-end my-4">
                     <select name="" id="" class="rounded-md">
-                            <option value="null" disabled>Report Type</option>
-                             <option value="" v-for="(item) in [{id:1,name:'Monthly Report'},{id:2,name:'Yearly Report'}]">
-                                    {{  item.name }}
-                             </option>
+                        <option value="null" disabled>Report Type</option>
+                        <option value="" v-for="(item) in [{ id: 1, name: 'Monthly Report' }, { id: 2, name: 'Yearly Report' }]">
+                            {{ item.name }}
+                        </option>
                     </select>
-              </div>
+                </div>
 
-                <div class="grid grid-cols-1 sm:grid-cols-3  md:grid-cols-3">
-                    <div>
+                <div class="max-w-7xl mx-auto">
+                    <div class="grid grid-cols-1 sm:grid-cols-2  md:grid-cols-2 gap-4">
+                    <div class="w-[80%]">
                         <PieChart :data="chart_data" />
                     </div>
-                    <div>
-                        <BarChart :data="chart_data" />
+                    <div class="flex flex-col">
+                        <div>
+                            <BarChart :data="chart_data" />
+                        </div>
+
+                        <div>
+                            <LineChart :data="chart_data" />
+                        </div>
                     </div>
 
-                    <div>
-                        <LineChart :data="chart_data" />
-                    </div>
+
+                </div>
                 </div>
             </div>
         </div>
